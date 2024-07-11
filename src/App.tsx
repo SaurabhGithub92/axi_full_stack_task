5d2060f3f08b0d00c5eb79b4aa535b6f1de6b7d4
saurabh <saurabh908@outlook.com>
2024-07-11 19:48:29 +0530

    Task completion with required ask

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,75 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
-interface State {
-  lastClicked?: Date;
-  buttonColor: "red" | "blue" | "green";
+interface ColorState {
+  lastClicked: "red" | "blue" | "green";
 }
 
-class App extends React.Component<object, State> {
-  constructor(props: object) {
-    super(props);
-    this.state = {
-      lastClicked: undefined,
-      buttonColor: "red",
-    };
+type ButtonColor = "red" | "blue" | "green";
+
+const App: React.FC = () => {
+  const getInitialColor = (): ButtonColor => {
+    const storedColor = localStorage.getItem('buttonColor');
+    return storedColor === "red" || storedColor === "blue" || storedColor === "green" ? storedColor : "red";
   }
 
-  public render() {
-    const { lastClicked, buttonColor } = this.state;
+  const [time, setTime] = useState<Date | null>(null);
+  const [colorState, setColorState] = useState<ColorState>({lastClicked: getInitialColor()});
 
-    return (
-      <>
-        <div>
-          <button
-            onClick={this.onClick}
-            style={{ backgroundColor: buttonColor }}
-          >
-            Click
-          </button>
-        </div>
-        <div className="TimeContainer">
-          <div className="TimeItem">
-            Local time:{" "}
-            {lastClicked !== undefined ? lastClicked.toString() : "Never"}
-          </div>
-          <div className="TimeItem">
-            GMT Time:{" "}
-            {lastClicked !== undefined ? lastClicked.toString() : "Never"}
-          </div>
-          <div className="TimeItem">
-            ACT Time:{" "}
-            {lastClicked !== undefined ? lastClicked.toString() : "Never"}
-          </div>
-        </div>
-      </>
-    );
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setTime(new Date());
+    }, 1000000);
+    return () => clearInterval(timer);
+  }, []);
+
+  const formattedTime = (time: Date | null, timeZone: string) => {
+    return time ? new Intl.DateTimeFormat(navigator.language, {
+      hour: 'numeric',
+      minute: 'numeric',
+      second: 'numeric',
+      timeZoneName: 'short',
+      timeZone,
+    }).format(time)
+    : "Loading time...";
   }
 
-  private onClick = () => {
-    this.setState({
-      lastClicked: new Date(),
-      buttonColor: this.getNextButtonColor(),
-    });
-  };
+  const toggleButtonColor = () => {
+    setColorState((prevState) => {
+      let newColor: ButtonColor;
+      switch (prevState.lastClicked) {
+        case "red":
+          newColor = "blue";
+          break;
+        case "blue": 
+          newColor = "green";
+          break;
+        case "green": 
+          newColor = "red";
+          break;                
+        default:
+          newColor = "red";
+      }
+      localStorage.setItem('buttonColor', newColor);
+      return {lastClicked: newColor};
+    })
+    setTime(new Date());
+  }
 
-  private getNextButtonColor = (): "red" | "blue" | "green" => {
-    switch (this.state.buttonColor) {
-      case "red":
-        return "blue";
-      case "blue":
-        return "green";
-      case "green":
-        return "red";
-      default:
-        throw new Error("Invalid color");
-    }
-  };
+  return (
+    <div className="App">
+      <button className={`ToggleButton ${colorState.lastClicked}`} onClick={toggleButtonColor}>
+        Toggle Time
+      </button>
+      <div className="TimeContainer">
+        <div className="TimeItem">Local Time : {formattedTime(time, Intl.DateTimeFormat().resolvedOptions().timeZone)}</div>
+
+        <div className="TimeItem">GMT Time : {formattedTime(time, 'GMT')}</div>
+
+        <div className="TimeItem">ACT Time : {formattedTime(time, 'ACT')}</div>
+      </div>
+    </div>
+  )
 }
 
 export default App;
